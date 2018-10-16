4b98c300d43b7fc610de5d6d10eff073fbab457b
MrYeL <mryelee@163.com>
2018-10-16 17:32:10 +0800

    充值

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -10,33 +10,41 @@ Page({
       'nickName': '二哈',
       'avatarUrl': '/images/order-details/icon-ddfh.png',
     },
-    functionDataDic: [{
-        'title': '当前积分',
-        'type': 0,
-        'btn': true,
-        'btnTitle': '每日签到'
-      },
-      {
-        'title': '我的订单',
-        'type': 1,
-        'btn': false,
-        'btnTitle':''
-      }, {
-        'title': '我的收货地址',
+    functionDataDic: [
+      [{
+          'title': '当前积分',
+          'type': 0,
+          'btn': true,
+          'btnTitle': '每日签到'
+        },
+        {
+          'title': '我的订单',
+          'type': 1,
+          'btn': false,
+          'btnTitle': ''
+        }, {
+          'title': '我的收货地址',
+          'type': 1,
+          'btn': false,
+          'btnTitle': ''
+        }, {
+          'title': '我的优惠券',
+          'type': 1,
+          'btn': false,
+          'btnTitle': ''
+        }, {
+          'title': '',
+          'type': 2,
+          'btn': true,
+          'btnTitle': '绑定手机号码'
+        }
+      ],
+      [{
+        'title': '关于我们',
         'type': 1,
         'btn': false,
         'btnTitle': ''
-      }, {
-        'title': '我的优惠券',
-        'type': 1,
-        'btn': false,
-        'btnTitle': ''
-      }, {
-        'title': '',
-        'type': 2,
-        'btn': true,
-        'btnTitle': '绑定手机号码'
-      }
+      }]
     ]
 
 
@@ -48,53 +56,56 @@ Page({
   onLoad: function(options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function() {
 
   },
-
   /**
-   * 页面相关事件处理函数--监听用户下拉动作
+   * 充值
    */
-  onPullDownRefresh: function() {
+  recharge: function() {
+    wx.navigateTo({
+      url: 'recharge/recharge',
+    })
+  },
+  /**
+   * 提现
+   */
+  withdraw: function() {
+    wx.navigateTo({
+      url: 'withDraw/withDraw',
+    })
+  },
+  /**
+   * 用户点击
+   */
+  itemClick: function(event) {
+
+    var clickIndex = event.currentTarget.dataset.index;
+    console.log(clickIndex);
 
   },
-
   /**
-   * 页面上拉触底事件的处理函数
+   * 签到/绑定手机号
    */
-  onReachBottom: function() {
+  btnClick: function (event) {
 
+    var clickIndex = event.currentTarget.dataset.index;
+    console.log(clickIndex);
+// 
+  },
+  /**
+   * 重新授权登录
+   */
+  reLogin: function () {
+
+    wx.showToast({
+      title: '重新授权登录',
+    })
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
 
-  }
+
 })
