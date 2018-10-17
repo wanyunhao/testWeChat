80899f572c1ae703dd8745d10a564ed88e3ffe6c
MrYeL <mryelee@163.com>
2018-10-17 14:23:19 +0800

    个人中心跳转

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -14,27 +14,33 @@ Page({
       [{
           'title': '当前积分',
           'type': 0,
+          'functionType': 0,
           'btn': true,
           'btnTitle': '每日签到'
         },
         {
           'title': '我的订单',
           'type': 1,
+          'functionType': 1,
+
           'btn': false,
           'btnTitle': ''
         }, {
           'title': '我的收货地址',
           'type': 1,
+          'functionType': 2,
           'btn': false,
           'btnTitle': ''
         }, {
           'title': '我的优惠券',
           'type': 1,
+          'functionType': 3,
           'btn': false,
           'btnTitle': ''
         }, {
           'title': '',
           'type': 2,
+          'functionType': 4,
           'btn': true,
           'btnTitle': '绑定手机号码'
         }
@@ -42,6 +48,7 @@ Page({
       [{
         'title': '关于我们',
         'type': 1,
+        'functionType': 5,
         'btn': false,
         'btnTitle': ''
       }]
@@ -54,12 +61,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
-  },
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
 
   },
   /**
@@ -83,23 +84,47 @@ Page({
    */
   itemClick: function(event) {
 
-    var clickIndex = event.currentTarget.dataset.index;
-    console.log(clickIndex);
+    var fType = event.currentTarget.dataset.functiontype;
+    console.log(fType);
+
+    var routePath = '';
+    switch (fType) {
+
+      case 1: //我的订单
+        routePath = 'order/order';
+        break;
+      case 2: //我的收货地址
+        routePath = 'address/address';
+
+        break;
+      case 3: //我的优惠券
+        routePath = 'coupon/coupon';
+        break;
+      default:
+        break;
+
+    };
+    if (routePath.length > 0) {
+      wx.navigateTo({
+        url: routePath,
+      })
+    }
+
 
   },
   /**
    * 签到/绑定手机号
    */
-  btnClick: function (event) {
-
-    var clickIndex = event.currentTarget.dataset.index;
+  btnClick: function(event) {
+    // console.log(event);
+    var clickIndex = event.currentTarget.dataset.functiontype;
     console.log(clickIndex);
-// 
+    
   },
   /**
    * 重新授权登录
    */
-  reLogin: function () {
+  reLogin: function() {
 
     wx.showToast({
       title: '重新授权登录',
