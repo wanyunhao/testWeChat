f5d74cac89226b1211af36b13cdc8365945ae0a8
MrYeL <mryelee@163.com>
2018-10-19 15:21:36 +0800

    添加Swiper

--- pages/mine/order/order.js
+++ pages/mine/order/order.js
@@ -15,53 +15,30 @@ Page({
   onLoad: function (options) {
     
   },
+  quickGuideAction:function (e) {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    var index = e.currentTarget.dataset.index;
+    this.setData({
+      currentType:index,
+    });
     
+    if (index==4){
+      this.setData({
+        orderList: ['数据1','数据2'],
+      })
+    }else {
+      this.setData({
+        orderList: null,
+      })
+    }
+    
+
+  },
+  bindchange: function(e) {
+
+
+
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
+  
 })
