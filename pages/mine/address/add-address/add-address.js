5ec8cb16b7566af3d79e15a1ad9c72eb77aa7091
MrYeL <mryelee@163.com>
2018-10-23 18:28:42 +0800

    地址选择

--- pages/mine/address/add-address/add-address.js
+++ pages/mine/address/add-address/add-address.js
@@ -1,4 +1,7 @@
 // pages/mine/address/add-address/add-address.js
+
+var addressData = require('../addressData/addressData.js');
+
 Page({
 
   /**
@@ -8,25 +11,50 @@ Page({
     initialData: [
       {
         title: '联系人',
-        placeholder: '姓名'
+        placeholder: '姓名',
+        'type':0,
       }, 
       {
         title: '手机号码',
-        placeholder: '11位手机号码'
+        placeholder: '11位手机号码',
+        'type': 0,
+
       }, 
       {
         title: '选择地址',
-        placeholder: '请选择'
+        placeholder: '请选择',
+        'type': 1,
+
       }, 
       {
         title: '详细地址',
-        placeholder: '街道门牌信息'
+        placeholder: '街道门牌信息',
+        'type': 0,
+
       }, 
       {
         title: '邮政编号',
-        placeholder: ''
+        placeholder: '',
+        'type': 0,
+
       }, 
-    ]
+    ],
+   addressInfo:{
+     provinces: ['1','2','3'],
+     citys: ['4', '5', '6'],
+     districts: ['7', '8', '9'],
+   },
+    selAddressInfo:{
+      selProvince: '',
+      selCity: '',
+      selDistrict: '',
+      selProvinceIndex: 0,
+      selCityIndex: 0,
+      selDistrictIndex: 0,
+    },
+    selType: 0,
+    mode:'selector',
+
   },
 
   /**
@@ -50,6 +78,14 @@ Page({
   cancleAction: function (e) {
     wx.navigateBack({})
   },
+  bindPickerAddressChange :function (e) {
+
+    this.setData ({
+      selType:1,
+      mode:'selector',
+    })
+    
+  }
 
  
 })
